d4672f0505c7262b6ad250066430e97eb03d8b1d
themaxtor <lucaluisetto91@gmail.com>
2020-03-07 22:59:02 +0000

    Delete unused functions and code

--- Robot_sketch/Robot_sketch.ino
+++ Robot_sketch/Robot_sketch.ino
@@ -1,6 +1,7 @@
 /*******************************************************************************************************
-* Test DC Motors Funtionality
-* Created 7/11/16
+* Robot Sketch
+* Science Hack Day @Tog
+* Created 07/03/20
 * 
 *
 *
@@ -67,57 +68,6 @@ void setMotors(MotorMode left, MotorMode right)
 //Define global variable
 long duration, distance;
 
-//Function to stop the wheels
-void wheels_stop()
-  {
-    analogWrite(motor_left[0], 0);
-    analogWrite(motor_left[1], 0);
-    analogWrite(motor_right[0], 0);
-    analogWrite(motor_right[1], 0);
-    delay(600);
-  }
-
-//Function to make the robot goes straight
-void go_straight(int speed1, int speed2)
-  {
-    analogWrite(motor_left[0], speed1);
-    analogWrite(motor_left[1], 0);
-    analogWrite(motor_right[0], 0);
-    analogWrite(motor_right[1], speed2);
-    delay(600);
-  }
-
-//Function to make the roboto goes back
-void go_back(int speed1, int speed2)
-  {
-    analogWrite(motor_left[0], 0);
-    analogWrite(motor_left[1], speed1);
-    analogWrite(motor_right[0], speed2);
-    analogWrite(motor_right[1], 0);
-    delay(600); 
-  }
-
-//Function to make the robot turns left
-void turn_left(int speed1)
-  {
-    analogWrite(motor_left[0], 0);
-    analogWrite(motor_left[1], 0);
-    analogWrite(motor_right[0], 0);
-    analogWrite(motor_right[1], speed1);
-    delay(600);
-  }
-
-//Function to make the roboto turns right
-void turn_right(int speed1)
-  {
-    analogWrite(motor_left[0], 0);
-    analogWrite(motor_left[1], speed1);
-    analogWrite(motor_right[0], 0);
-    analogWrite(motor_right[1], 0);
-    delay(600);
-  }
-
-
 
 void move_servo(Servo object, int desired_angle)
 {
@@ -179,7 +129,7 @@ void setup() {
   } 
 
   //Rotate servo from 180 to 0
-  for(angle = 180; angle > 0; angle -= 1)    // command to move from 0 degrees to 180 degrees 
+  for(angle = 180; angle > 0; angle -= 1)    // command to move from 180 degrees to 0 degrees 
   {                                  
     Servo1.write(angle);                 //command to rotate the servo to the specified angle
     delay(15);                       
@@ -225,51 +175,7 @@ void loop()
 		case 7: setMotors(BACKWARD, BACKWARD); break;
 		case 8: setMotors(BACKWARD, STOP); break;
 	}
+  
 
-#if 0
-  Serial.print("Actual Distance: ");
-  int distance = read_distance();
-  Serial.println(distance);
-  //if distance > 15 go straight
-  if (distance > 15) 
-  {
-    Serial.println("Go straight");
-    go_straight(255,255);
-  }
-  else
-  {
-    Serial.println("Stop Wheels");
-    wheels_stop();
-    move_servo(Servo1, 0);
-    Serial.print("Distance from right object: ");
-    distance = read_distance();
-    Serial.println(distance);
-    if (distance > 15)
-    {
-      Serial.println("Go right");
-      turn_right(255);
-      move_servo(Servo1, 70);
-    }
-    else
-    {
-      move_servo(Servo1, 170);
-      Serial.print("Distance from left object: ");
-      distance = read_distance();
-      Serial.println(distance);
-      if (distance > 15)
-      {
-        Serial.println("Go left");
-        turn_left(255);
-        move_servo(Servo1, 70);
-      }
-      else
-      {
-        Serial.println("Go back");
-        go_back(255,255);
-        move_servo(Servo1, 70);
-      }
-    }
-  }
-#endif
   delay(100);
 }
